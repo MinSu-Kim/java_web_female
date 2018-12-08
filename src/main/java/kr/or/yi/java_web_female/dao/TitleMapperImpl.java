3b59e291c2212e617e9dcce501d6523aa3df01e3
psw2701 <psw2701@naver.com>
2018-12-08 13:36:33 +0900

    Title test완료

--- src/main/java/kr/or/yi/java_web_female/dao/TitleMapperImpl.java
+++ src/main/java/kr/or/yi/java_web_female/dao/TitleMapperImpl.java
@@ -1,18 +1,63 @@
 package kr.or.yi.java_web_female.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import kr.or.yi.java_web_female.dto.Title;
 import kr.or.yi.java_web_female.jdbc.MyBatisSqlSessionFactory;
 
-public class TitleMapperImpl implements TitleMapper{
+public class TitleMapperImpl implements TitleMapper {
 	private static final String namespace = "kr.or.yi.java_web_female.dao.TitleMapper";
-	
+
 	@Override
 	public Title selectTitleByNo(Title title) {
 		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
 			return sqlSession.selectOne(namespace + ".selectTitleByNo", title);
 		}
 	}
+
+	@Override
+	public List<Title> selectTitleByAll() {
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			return sqlSession.selectList(namespace + ".selectTitleByAll");
+		}
+	}
+
+	@Override
+	public int insertTitle(Title title) {
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			int res = sqlSession.insert(namespace + ".insertTitle", title);
+			sqlSession.commit();
+			return res;
+		}
+	}
+
+	@Override
+	public int updateTitle(Title title) {
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			int res = sqlSession.update(namespace + ".updateTitle", title);
+			sqlSession.commit();
+			return res;
+		}
+	}
+
+	@Override
+	public int deleteTitle(Title title) {
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			int res = sqlSession.delete(namespace + ".deleteTitle", title);
+			sqlSession.commit();
+			return res;
+		}
+	}
+
+	
+	
+
+	
+	
+
+
+
 
 }
--- src/test/java/kr/or/yi/java_web_female/TitleMapperTest.java
+++ src/test/java/kr/or/yi/java_web_female/TitleMapperTest.java
@@ -1,5 +1,7 @@
 package kr.or.yi.java_web_female;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -18,10 +20,50 @@ public class TitleMapperTest  extends AbstractTest{
 	public void test01selectTitleByNo() {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Title searchTit = new Title();
-		searchTit.setCode("T0001");
+		searchTit.setCode("T001");
 		
 		Title searchedTit = dao.selectTitleByNo(searchTit);
 		Assert.assertNotNull(searchedTit);
 	}
+	
+	@Test
+	public void test02selectTitleByAll() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Title searchTit = new Title();
+		searchTit.setCode("T001");
+		List<Title> titList = dao.selectTitleByAll();
+		Assert.assertNotNull(titList);
+	}
+	
+	/*@Test
+	public void test03insertTitle() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Title title = new Title();
+		title.setCode("T003");
+		title.setName("인턴");
+		title.settGrant(false);
+		int res = dao.insertTitle(title);
+		Assert.assertEquals(1, res);
+	}
+	
+	@Test
+	public void test04updateTitle() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Title title = new Title();
+		title.setCode("T003");
+		title.setName("알바");
+		title.settGrant(false);
+		
+		int res = dao.updateTitle(title);
+		Assert.assertSame(1, res);
 
+	}*/
+	@Test
+	public void test05deleteTitle() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Title title = new Title();
+		title.setCode("T003");
+		int res = dao.deleteTitle(title);
+		Assert.assertEquals(1, res);
+	}
 }
