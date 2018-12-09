8a152af2a2b53da7add2d085c1d02f140e566bbd
bmkim621 <moonstruck0614@gmail.com>
2018-12-09 22:43:14 +0900

    Rent(Insert 테스트), dto 패키지에 Insurance, Rent 추가

---
+++ src/main/java/kr/or/yi/java_web_female/dto/Insurance.java
@@ -0,0 +1,54 @@
+package kr.or.yi.java_web_female.dto;
+
+public class Insurance {
+	private String code;	//보험코드
+	private String carType;	//차종(경차, 소형, 중형, ..)
+	private int price;	//보험료
+	
+	public Insurance() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Insurance(String code, String carType, int price) {
+		super();
+		this.code = code;
+		this.carType = carType;
+		this.price = price;
+	}
+
+	public Insurance(String code) {
+		super();
+		this.code = code;
+	}
+
+	//getter, setter
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getCarType() {
+		return carType;
+	}
+
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return String.format("Insurance [%s, %s, %s]", code, carType, price);
+	}	
+}
