384736072fb1db7b2c1e3410388bca4c4ce32951
mia94 <mia_3@hanmail.net>
2018-12-11 14:46:37 +0900

    addOption오류수정

---
+++ src/main/java/kr/or/yi/java_web_female/dao/CarModelMapper.java
@@ -0,0 +1,10 @@
+package kr.or.yi.java_web_female.dao;
+
+import java.util.List;
+
+import kr.or.yi.java_web_female.dto.CarModel;
+
+public interface CarModelMapper {
+	CarModel selectCarModelByNo(CarModel carModel);
+	List<CarModel> selectCarModelByAll();
+}
