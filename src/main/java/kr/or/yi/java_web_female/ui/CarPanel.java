79c1a8fa999b93fdf62a97ea3b3e860cd46d804c
mia94 <mia_3@hanmail.net>
2018-12-11 17:16:49 +0900

    CarPanelUi

--- src/main/java/kr/or/yi/java_web_female/ui/CarPanel.java
+++ src/main/java/kr/or/yi/java_web_female/ui/CarPanel.java
@@ -2,40 +2,76 @@ package kr.or.yi.java_web_female.ui;
 
 import javax.swing.JPanel;
 
+import kr.or.yi.java_web_female.dto.Brand;
 import kr.or.yi.java_web_female.dto.CarType;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 @SuppressWarnings("serial")
 public class CarPanel extends JPanel {
+	private JTextField tfName;
+	private JTextField tfCode;
 
 	public CarPanel() {
 		setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		add(panel, BorderLayout.NORTH);
-		panel.setLayout(new GridLayout(0, 2, 0, 0));
+		JPanel panelSelect = new JPanel();
+		add(panelSelect, BorderLayout.NORTH);
+		panelSelect.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		ComboPanel<CarType> panel_1 = new ComboPanel<>();
-//		panel.setTitle("CarType");
-		panel.add(panel_1);
+		ComboPanel<CarType> panelCarType = new ComboPanel<>();
+		panelCarType.setTitle("CarType");
+		List<CarType> arr = new ArrayList<>();
+
+		panelSelect.add(panelCarType);
 		
-		ComboPanel panel_3 = new ComboPanel();
-		panel.add(panel_3);
+		ComboPanel<Brand> panelBrand = new ComboPanel<>();
+		panelBrand.setTitle("Brand");
+		panelSelect.add(panelBrand);
 		
-		JPanel panel_2 = new JPanel();
-		add(panel_2, BorderLayout.CENTER);
+		JPanel panelName = new JPanel();
+		panelSelect.add(panelName);
+		panelName.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JPanel panel_4 = new JPanel();
-		add(panel_4, BorderLayout.SOUTH);
+		JLabel lblName = new JLabel("Name");
+		lblName.setHorizontalAlignment(SwingConstants.CENTER);
+		panelName.add(lblName);
+		
+		tfName = new JTextField();
+		panelName.add(tfName);
+		tfName.setColumns(10);
+		
+		JPanel panelCode = new JPanel();
+		panelSelect.add(panelCode);
+		panelCode.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JLabel lblCode = new JLabel("CarCode");
+		lblCode.setHorizontalAlignment(SwingConstants.CENTER);
+		panelCode.add(lblCode);
+		
+		tfCode = new JTextField();
+		panelCode.add(tfCode);
+		tfCode.setColumns(10);
+		
+		JPanel panelList = new JPanel();
+		add(panelList, BorderLayout.CENTER);
+		
+		JPanel panelBtn = new JPanel();
+		add(panelBtn, BorderLayout.SOUTH);
 		
 		JButton btnOk = new JButton("선택");
-		panel_4.add(btnOk);
+		panelBtn.add(btnOk);
 		
 		JButton btnCancel = new JButton("취소");
-		panel_4.add(btnCancel);
+		panelBtn.add(btnCancel);
 
 	}
 
