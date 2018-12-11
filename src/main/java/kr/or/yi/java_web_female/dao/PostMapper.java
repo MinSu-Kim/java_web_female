effcb3cb609ebca5e0acab92cb68f08cbe018d27
psw2701 <psw2701@naver.com>
2018-12-11 14:41:06 +0900

    ui이름 변경

--- src/main/java/kr/or/yi/java_web_female/dao/PostMapper.java
+++ src/main/java/kr/or/yi/java_web_female/dao/PostMapper.java
@@ -5,6 +5,6 @@ import java.util.List;
 import kr.or.yi.java_web_female.dto.Post;
 
 public interface PostMapper {
-	Post selectPostByNo (Post post);
-//	List<Post> selectPostByAll();
+	List<Post> selectPostByName ();
+	List<Post> selectPostByAll();
 }
