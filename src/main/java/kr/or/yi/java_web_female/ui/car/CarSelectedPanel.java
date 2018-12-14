9ba782dedd4a334cd574c6d323bb73ea1596021e
mia94 <mia_3@hanmail.net>
2018-12-14 16:24:16 +0900

    CarSelectedPanel

--- src/main/java/kr/or/yi/java_web_female/ui/car/CarSelectedPanel.java
+++ src/main/java/kr/or/yi/java_web_female/ui/car/CarSelectedPanel.java
@@ -4,8 +4,15 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import javax.swing.JButton;
+import kr.or.yi.java_web_female.ui.ComboPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import kr.or.yi.java_web_female.ui.list.CarPriceList;
 
 public class CarSelectedPanel extends JPanel {
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Create the panel.
@@ -26,6 +33,52 @@ public class CarSelectedPanel extends JPanel {
 		
 		JPanel panel_info = new JPanel();
 		panel_2.add(panel_info);
+		panel_info.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel panelCode = new JPanel();
+		panel_info.add(panelCode);
+		panelCode.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("차량코드");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		panelCode.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		panelCode.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panelName = new JPanel();
+		panel_info.add(panelName);
+		panelName.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JLabel lblNewLabel_2 = new JLabel("모델명");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		panelName.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		panelName.add(textField_1);
+		textField_1.setColumns(10);
+		
+		
+		ComboPanel panelBrand = new ComboPanel();
+		panelBrand.setTitle("브랜드");
+		panel_info.add(panelBrand);
+		
+		ComboPanel panelCarType = new ComboPanel();
+		panelCarType.setTitle("차종");
+		panel_info.add(panelCarType);
+		
+		CarPriceList panelPrice = new CarPriceList();
+		panel_info.add(panelPrice);
+		
+		JPanel panelRentCnt = new JPanel();
+		panel_info.add(panelRentCnt);
+		panelRentCnt.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panelRentCnt.add(lblNewLabel);
 		
 		JPanel panelBtn = new JPanel();
 		panel.add(panelBtn, BorderLayout.SOUTH);
---
+++ src/main/java/kr/or/yi/java_web_female/ui/list/CarPriceList.java
@@ -0,0 +1,71 @@
+package kr.or.yi.java_web_female.ui.list;
+
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import kr.or.yi.java_web_female.dto.CarModel;
+
+public class CarPriceList extends AbstractListPanel<CarModel> {
+
+	public CarPriceList() {
+
+	}
+
+	@Override
+	protected void setAlignWidth() {
+		tableCellAlignment(SwingConstants.CENTER, 0, 1, 2, 3, 4);
+		tableSetWidth(100, 100, 100, 100, 100);
+	}
+
+	@Override
+	protected String[] getColumnNames() {
+		return new String[] {"기본비용","6시간","10시간","12시간","12시간초과"};
+	}
+
+	@Override
+	protected Object[] getItemRows(CarModel item) {
+		return new Object[] {
+				item.getBasicCharge(),item.getHour6(),item.getHour10(),item.getHour12(),item.getHourElse()
+		};
+	}
+
+	@Override
+	protected CarModel getItem(int selectedIndex) {
+		int basicCharge = (int)table.getValueAt(selectedIndex, 0);
+		int hour6 = (int)table.getValueAt(selectedIndex, 1);
+		int hour10 = (int)table.getValueAt(selectedIndex, 2);
+		int hour12 = (int)table.getValueAt(selectedIndex, 3);
+		int hourElse = (int)table.getValueAt(selectedIndex, 4);
+		
+		CarModel model = new CarModel();
+		model.setBasicCharge(basicCharge);
+		model.setHour6(hour6);
+		model.setHour10(hour10);
+		model.setHour12(hour12);
+		model.setHourElse(hourElse);
+		return model;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
