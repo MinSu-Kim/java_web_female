446dd7dcc1d334e612867e7010b30b83a804ab19
psw2701 <psw2701@naver.com>
2018-12-18 13:37:34 +0900

    event ui 하는중

--- src/main/java/kr/or/yi/java_web_female/ui/ManagerPanel.java
+++ src/main/java/kr/or/yi/java_web_female/ui/ManagerPanel.java
@@ -28,43 +28,35 @@ public class ManagerPanel extends JPanel {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		panel.add(tabbedPane, BorderLayout.CENTER);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setToolTipText("");
-		tabbedPane.addTab("고객관리", null, panel_1, null);
+		JPanel panel_01 = new JPanel();
+		tabbedPane.addTab("고객관리", null, panel_01, null);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setToolTipText("");
-		tabbedPane.addTab("개인정보관리", null, panel_2, null);
+		JPanel panel_02 = new JPanel();
+		tabbedPane.addTab("개인정보관리", null, panel_02, null);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setToolTipText("");
-		tabbedPane.addTab("사원관리", null, panel_3, null);
+		JPanel panel_03 = new JPanel();
+		tabbedPane.addTab("사원관리", null, panel_03, null);
 		
-		JPanel panel_4 = new JPanel();
-		panel_4.setToolTipText("");
-		tabbedPane.addTab("등급관리", null, panel_4, null);
+		JPanel panel_04 = new JPanel();
+		tabbedPane.addTab("등급&이벤트관리", null, panel_04, null);
+			
+		CarPanel panel_05 = new CarPanel();
+		tabbedPane.addTab("차량관리", null, panel_05, null);
 		
-		JPanel panel_5 = new JPanel();
-		panel_5.setToolTipText("");
-		tabbedPane.addTab("이벤트관리", null, panel_5, null);
+		CarManagementPanel panel_06 = new CarManagementPanel();
+		tabbedPane.addTab("차량옵션관리", null, panel_06, null);
 		
-		CarPanel panel_6 = new CarPanel();
-		tabbedPane.addTab("차량관리", null, panel_6, null);
+		RentPanel panel_07 = new RentPanel();
+		tabbedPane.addTab("대여관리", null, panel_07, null);
 		
-		CarManagementPanel panel_7 = new CarManagementPanel();
-		tabbedPane.addTab("차량옵션관리", null, panel_7, null);
+		JPanel panel_08 = new JPanel();
+		tabbedPane.addTab("대여현황", null, panel_08, null);
 		
-		RentPanel panel_8 = new RentPanel();
-		tabbedPane.addTab("대여관리", null, panel_8, null);
-		
-		JPanel panel_9 = new JPanel();
-		tabbedPane.addTab("대여현황", null, panel_9, null);
+		JPanel panel_09 = new JPanel();
+		tabbedPane.addTab("반납관리", null, panel_09, null);
 		
 		JPanel panel_10 = new JPanel();
-		tabbedPane.addTab("반납관리", null, panel_10, null);
-		
-		JPanel panel_11 = new JPanel();
-		tabbedPane.addTab("전체통계", null, panel_11, null);
+		tabbedPane.addTab("전체통계", null, panel_10, null);
 		
 		
 
