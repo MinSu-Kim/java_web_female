852f0b86b998a57e0fb70a07147fe3cde5dcbd1c
mia94 <mia_3@hanmail.net>
2018-12-22 15:43:51 +0900

    carmodel, usepic 개수 맞춰둠

---
+++ src/main/java/kr/or/yi/java_web_female/ui/car/MyTableCellRenderer.java
@@ -0,0 +1,19 @@
+package kr.or.yi.java_web_female.ui.car;
+
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+
+public class MyTableCellRenderer implements TableCellRenderer {
+	
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		
+	
+		return null;
+	}
+
+
+}
