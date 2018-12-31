9703a9fe167347447da0b27a68806c85e8366c8c
mia94 <mia_3@hanmail.net>
2018-12-31 14:09:02 +0900

    관리자로그인 버그 수정됨

--- src/main/java/kr/or/yi/java_web_female/chart_panel/CarChartPanel.java
+++ src/main/java/kr/or/yi/java_web_female/chart_panel/CarChartPanel.java
@@ -2,7 +2,12 @@ package kr.or.yi.java_web_female.chart_panel;
 
 import java.awt.GridLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JRadioButton;
+
+import com.jtattoo.plaf.graphite.GraphiteRootPaneUI;
 
 @SuppressWarnings("serial")
 public class CarChartPanel extends JPanel {
@@ -11,10 +16,14 @@ public class CarChartPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public CarChartPanel() {
-		setLayout(new GridLayout(2, 1, 0, 0));
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		add(panel_2);
+		panel_2.setLayout(new GridLayout(2, 2, 0, 0));
 		
 		JPanel panelCar = new JPanel();
-		add(panelCar);
+		panel_2.add(panelCar);
 		panelCar.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		BarChartCarModel panel_BarChart = new BarChartCarModel();
@@ -24,7 +33,7 @@ public class CarChartPanel extends JPanel {
 		panelCar.add(panel_PieChart);
 		
 		JPanel panelCar2 = new JPanel();
-		add(panelCar2);
+		panel_2.add(panelCar2);
 		panelCar2.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		PieChartBrand panel = new PieChartBrand();
@@ -32,6 +41,27 @@ public class CarChartPanel extends JPanel {
 		
 		LineChartRentCount panel_1 = new LineChartRentCount();
 		panelCar2.add(panel_1);
+		
+		JPanel panelBtn = new JPanel();
+		add(panelBtn, BorderLayout.NORTH);
+		
+		ButtonGroup group = new ButtonGroup();
+		
+		JRadioButton rdbtnCarModel = new JRadioButton("차량별 대여회수");
+		group.add(rdbtnCarModel);
+		panelBtn.add(rdbtnCarModel);
+		
+		JRadioButton rdbtnCarType = new JRadioButton("차종별 보유개수");
+		group.add(rdbtnCarType);
+		panelBtn.add(rdbtnCarType);
+		
+		JRadioButton rdbtnBrand = new JRadioButton("브랜드별 보유개수");
+		group.add(rdbtnBrand);
+		panelBtn.add(rdbtnBrand);
+		
+		JRadioButton rdbtnRentCount = new JRadioButton("차종별 월 렌트 수");
+		group.add(rdbtnRentCount);
+		panelBtn.add(rdbtnRentCount);
 
 	}
 
