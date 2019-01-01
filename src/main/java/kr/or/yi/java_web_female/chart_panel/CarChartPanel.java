d552ddaa9ea4faa1960ae4ef888be11c2428b9fe
mia94 <mia_3@hanmail.net>
2019-01-01 15:25:41 +0900

    라인차트오류 해결

--- src/main/java/kr/or/yi/java_web_female/chart_panel/CarChartPanel.java
+++ src/main/java/kr/or/yi/java_web_female/chart_panel/CarChartPanel.java
@@ -24,8 +24,7 @@ public class CarChartPanel extends JPanel implements ActionListener{
 	private JRadioButton rdbtnBrand;
 	private JRadioButton rdbtnCarModel;
 	//패널
-	private JPanel panelAllChart;
-//	private JPanel panelSelect;
+	private JPanel panelChart;
 	private JPanel panelSelectedOne;
 	private JPanel panelTotal;
 	//차트4가지
@@ -42,28 +41,28 @@ public class CarChartPanel extends JPanel implements ActionListener{
 		
 		panelTotal = new JPanel();
 		panelTotal.setBorder(new EmptyBorder(15, 15, 15, 15));
-		add(panelTotal);
+		add(panelTotal, BorderLayout.CENTER);
 		panelTotal.setLayout(new BorderLayout(0, 0));
 		
-		panelAllChart = new JPanel();
-		panelTotal.add(panelAllChart, BorderLayout.CENTER);
-		panelAllChart.setLayout(new GridLayout(0, 2, 15, 15));
+		panelChart = new JPanel();
+		panelTotal.add(panelChart, BorderLayout.CENTER);
+		panelChart.setLayout(new GridLayout(0, 2, 15, 15));
 		
 		panel_BarChart = new BarChartCarModel();
 		panel_BarChart.getpBarChart().setBorder(new EmptyBorder(0, 0, 0, 20));
-		panelAllChart.add(panel_BarChart);
+		panelChart.add(panel_BarChart);
 		
 		panel_PieCarType = new PieChartCarType();
 		panel_PieCarType.getpPieChart().setBorder(new EmptyBorder(0, 0, 0, 20));
-		panelAllChart.add(panel_PieCarType);
+		panelChart.add(panel_PieCarType);
 		
 		panel_PieBrand = new PieChartBrand();
 		panel_PieBrand.getpPieChart().setBorder(new EmptyBorder(0, 0, 0, 20));
-		panelAllChart.add(panel_PieBrand);
+		panelChart.add(panel_PieBrand);
 		
 		panel_LineRentCount = new LineChartRentCount();
 		panel_LineRentCount.getpLineChart().setBorder(new EmptyBorder(0, 0, 0, 20));
-		panelAllChart.add(panel_LineRentCount);
+		panelChart.add(panel_LineRentCount);
 		
 		panelSelectedOne = new JPanel();
 		add(panelSelectedOne, BorderLayout.SOUTH);
@@ -123,18 +122,21 @@ public class CarChartPanel extends JPanel implements ActionListener{
 	
 	protected void do_rdbtnTotal_actionPerformed(ActionEvent e) {
 		//전체보기 선택
-		panelAllChart.setVisible(true);
+		panelTotal.setVisible(true);
 		panelSelectedOne.setVisible(false);
+		panelTotal.add(panelChart, BorderLayout.CENTER);
+		add(panelTotal, BorderLayout.CENTER);
+		add(panelSelectedOne, BorderLayout.SOUTH);
 		
 		panel_BarChart.getpBarChart().getBarChart().setPrefSize(440,230);
 		panel_PieCarType.getpPieChart().getPieChart().setPrefSize(440, 250);
 		panel_PieBrand.getpPieChart().getPieChart().setPrefSize(440, 250);
 		panel_LineRentCount.getpLineChart().getLineChart().setPrefSize(440, 250);
 		//네개 다시 붙여넣기
-		panelAllChart.add(panel_BarChart);
-		panelAllChart.add(panel_PieCarType);
-		panelAllChart.add(panel_PieBrand);
-		panelAllChart.add(panel_LineRentCount);
+		panelChart.add(panel_BarChart);
+		panelChart.add(panel_PieCarType);
+		panelChart.add(panel_PieBrand);
+		panelChart.add(panel_LineRentCount);
 	}
 	
 	protected void do_rdbtnRentCount_actionPerformed(ActionEvent e) {
@@ -161,8 +163,12 @@ public class CarChartPanel extends JPanel implements ActionListener{
 	}
 
 	private void addReplaceChartPanel(JPanel panel) {
-		panelAllChart.setVisible(false);
+		panelTotal.setVisible(false);
 		panelSelectedOne.setVisible(true);
+		panelSelectedOne.add(panelChart, BorderLayout.CENTER);
+		add(panelSelectedOne, BorderLayout.CENTER);
+		panelTotal.add(panelChart, BorderLayout.SOUTH);
+		add(panelTotal, BorderLayout.SOUTH);
 
 		panelSelectedOne.removeAll();
 		panelSelectedOne.add(panel);
