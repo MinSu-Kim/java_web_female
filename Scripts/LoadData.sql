2fe4f10e809d8ac66d332fcc44b48daea79b8680
psw2701 <psw2701@naver.com>
2019-01-04 01:47:33 +0900

    commit

--- Scripts/LoadData.sql
+++ Scripts/LoadData.sql
@@ -21,4 +21,4 @@ set zipcode=@zipcode, sido=@sido, sigungu=@sigungu, eupmyeon=@eupmyeon, doro=@do
 create index idx_post_doro on post(doro);*/
 
 
--- C:/program/workspace-proj/java_web_female/DataFiles/대구광역시.txt
+-- load data local infile 'C:/program/workspace-proj/java_web_female/DataFiles/대구광역시.txt' into table post
